5c9a9846ad22c2655ea36d509b89356fef361027
qPexLegendary <terentev.andrey.2002@gmail.com>
2021-05-23 13:15:30 +0700

    Добавлен вспомогательный класс, хранящий тройку

---
+++ src/ru/dragonestia/apocalypse/util/Three.java
@@ -0,0 +1,14 @@
+package ru.dragonestia.apocalypse.util;
+
+public class Three<T, V, U> {
+
+    public T first;
+    public V second;
+    public U third;
+
+    public Three(T first, V second, U third){
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
