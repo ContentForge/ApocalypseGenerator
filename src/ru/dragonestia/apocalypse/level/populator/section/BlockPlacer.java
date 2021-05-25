972529c16ba4911b20670518dc1b8a8ed559a8d6
qPexLegendary <terentev.andrey.2002@gmail.com>
2021-05-25 20:09:59 +0700

    Теперь метод установки блока возвращает id предыдущего блока

--- src/ru/dragonestia/apocalypse/level/populator/section/BlockPlacer.java
+++ src/ru/dragonestia/apocalypse/level/populator/section/BlockPlacer.java
@@ -2,6 +2,6 @@ package ru.dragonestia.apocalypse.level.populator.section;
 
 public interface BlockPlacer {
 
-    void setBlock(int x, int y, int z, int id, int meta);
+    int setBlock(int x, int y, int z, int id, int meta);
 
 }
--- src/ru/dragonestia/apocalypse/level/populator/section/HouseSection.java
+++ src/ru/dragonestia/apocalypse/level/populator/section/HouseSection.java
@@ -16,23 +16,31 @@ public abstract class HouseSection extends Section {
         int r = random.nextInt(4);
 
         BlockPlacer blockPlacer = (x, y, z, id, meta) -> {
+            int lastId;
             switch (r % 4) {
                 case 0:
+                    lastId = chunk.getBlockId(x, y, z);
                     chunk.setBlock(x, y, z, id, meta);
-                    return;
+                    break;
 
                 case 1:
+                    lastId = chunk.getBlockId(15 - z, y, x);
                     chunk.setBlock(15 - z, y, x, id, meta);
-                    return;
+                    break;
 
                 case 2:
+                    lastId = chunk.getBlockId(15 - x, y, 15 - z);
                     chunk.setBlock(15 - x, y, 15 - z, id, meta);
-                    return;
+                    break;
 
                 case 3:
+                    lastId = chunk.getBlockId(z, y, 15 - x);
                     chunk.setBlock(z, y, 15 - x, id, meta);
                     break;
+                default:
+                    lastId = -1;
             }
+            return lastId;
         };
 
         generate(blockPlacer, chunk, chunk.getBiomeId(5, 5) != ApocalypseGenerator.COMMON_BIOME);
