369f0c558f025ccbba56709ed46801a5789c5135
qPexLegendary <terentev.andrey.2002@gmail.com>
2021-06-06 10:53:49 +0700

    Изменена генерация разрушенностей зданий

--- src/ru/dragonestia/apocalypse/level/populator/section/BrokenHouse.kt
+++ src/ru/dragonestia/apocalypse/level/populator/section/BrokenHouse.kt
@@ -3,6 +3,7 @@ package ru.dragonestia.apocalypse.level.populator.section
 import cn.nukkit.item.Item
 import cn.nukkit.level.ChunkManager
 import cn.nukkit.level.format.FullChunk
+import ru.dragonestia.apocalypse.level.populator.section.filter.VoidFilter
 import java.util.*
 
 class BrokenHouse(random: Random, chunkManager: ChunkManager) : HouseSection(random, chunkManager) {
@@ -23,7 +24,8 @@ class BrokenHouse(random: Random, chunkManager: ChunkManager) : HouseSection(ran
                 if(random.nextFloat() < 0.6f) walls[x][z] = true
             }
         }
-        val void = if(broken && random.nextFloat() < 0.6f) random.nextInt(40) + 60 else -1
+        val levels = (2 + random.nextInt(15))
+        val voidFilter = VoidFilter(h, h + levels * 4, random.nextInt(3), random)
 
         var tempY = h - 1
         while(true) {
@@ -32,39 +34,39 @@ class BrokenHouse(random: Random, chunkManager: ChunkManager) : HouseSection(ran
                 for (z in 0..15) {
                     if(!walls[x][z]) continue
 
-                    if(placeWall(x, tempY, z, placer, broken, void) == 0) next = true
+                    if(placeWall(x, tempY, z, placer, broken) == 0) next = true
                 }
             tempY--
             if(!next) break
         }
 
-        for(l in 0..(2 + random.nextInt(15))){
-            generateLevel(placer, h + l * 4, chunk, broken, walls, void)
+        for(l in 0..levels){
+            generateLevel(placer, h + l * 4, broken, walls, voidFilter)
         }
     }
 
-    private fun generateLevel(placer: BlockPlacer, dy: Int, chunk: FullChunk, broken: Boolean, walls: Array<BooleanArray>, void: Int) {
+    private fun generateLevel(placer: BlockPlacer, dy: Int, broken: Boolean, walls: Array<BooleanArray>, voidFilter: VoidFilter) {
         for(y in 0..3){
             for(x in 0..15){
                 for(z in 0..15){
+                    if(voidFilter.check(x, dy, z)) continue
                     if(x in 1..14 && z in 1..14 && y == 0){
-                        placeFloor(x, y + dy, z, placer, broken, void)
+                        placeFloor(x, y + dy, z, placer, broken)
                         continue
                     }
 
                     if(y == 0){
-                        placeBorder(x, y + dy, z, placer, broken, void)
+                        placeBorder(x, y + dy, z, placer, broken)
                         continue
                     }
 
-                    if(walls[x][z]) placeWall(x, y + dy, z, placer, broken, void)
+                    if(walls[x][z]) placeWall(x, y + dy, z, placer, broken)
                 }
             }
         }
     }
 
-    private fun placeFloor(x: Int, y: Int, z: Int, placer: BlockPlacer, broken: Boolean, void: Int){
-        if(void != -1 && inVoid(x, y, z, void)) return
+    private fun placeFloor(x: Int, y: Int, z: Int, placer: BlockPlacer, broken: Boolean){
         val block = when(random.nextInt(2 + (if(broken) 1 else 0))){
             0 -> Pair(1, 0)
             1 -> Pair(Item.SLAB, 3)
@@ -73,8 +75,7 @@ class BrokenHouse(random: Random, chunkManager: ChunkManager) : HouseSection(ran
         placer.setBlock(x, y, z, block.first, block.second)
     }
 
-    private fun placeBorder(x: Int, y: Int, z: Int, placer: BlockPlacer, broken: Boolean, void: Int){
-        if(void != -1 && inVoid(x, y, z, void)) return
+    private fun placeBorder(x: Int, y: Int, z: Int, placer: BlockPlacer, broken: Boolean){
         val block = when(random.nextInt(4 + (if(broken) 1 else 0))){
             0 -> Pair(Item.STONE_BRICKS, 0)
             1 -> Pair(Item.STONE_BRICKS, 2)
@@ -84,8 +85,7 @@ class BrokenHouse(random: Random, chunkManager: ChunkManager) : HouseSection(ran
         placer.setBlock(x, y, z, block.first, block.second)
     }
 
-    private fun placeWall(x: Int, y: Int, z: Int, placer: BlockPlacer, broken: Boolean, void: Int): Int {
-        if(void != -1 && inVoid(x, y, z, void)) return -1
+    private fun placeWall(x: Int, y: Int, z: Int, placer: BlockPlacer, broken: Boolean): Int {
         val block = when(random.nextInt(5 + (if(broken) 1 else 0))){
             0 -> Pair(Item.STONE_BRICKS, 0)
             1 -> Pair(Item.STONE_BRICKS, 2)
@@ -94,14 +94,6 @@ class BrokenHouse(random: Random, chunkManager: ChunkManager) : HouseSection(ran
             else -> Pair(0, 0)
         }
         return placer.setBlock(x, y, z, block.first, block.second)
-    }
-
-    private fun inVoid(x: Int, y: Int, z: Int, dy: Int): Boolean {
-        val dX = (x - 15) / (if(dy > 70) 3 else 2)
-        val dY = (y - dy) / (if(dy > 70) 6 else 5)
-        val dZ = z - 15 / (if(dy > 70) 2 else 1)
-
-        return dX*dX + dY*dY + dZ*dZ <= 10
     }
 
 }
